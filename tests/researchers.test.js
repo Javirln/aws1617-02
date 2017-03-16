5d272d622c56c15f480c3f36736f81db57efb9ae
Javier Rodriguez <javier.rodleo@gmail.com>
2017-03-16 10:21:25 +0000

    Adds tests and travis support

---
+++ tests/researchers.test.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const chai = require('chai');
+chai.use(require('chai-things'));
+const expect = chai.expect;
+
+describe('Fist test', () => {
+    it('should pass the addition of two numbers', (done) => {
+        const sum = 1 + 3;
+        
+        expect(sum).to.be.equal(4);
+        expect(sum).to.be.not.equal(3);
+        done();
+    });
+});
