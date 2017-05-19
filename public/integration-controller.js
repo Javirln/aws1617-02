fa23d681d3195c59e4d3a579c5e6e7dd1bab406a
David Corral <davidcorralp94@gmail.com>
2017-05-19 12:06:27 +0000

    More integration details

--- public/integration-controller.js
+++ public/integration-controller.js
@@ -43,7 +43,10 @@ angular.module("ResearcherListApp").controller("IntCtrl", function($scope, $http
         $scope.projects = [];
         $scope.selectedProject = null;
         $scope.researchers = [];
-
+        $("#modal-header").empty();
+        $("#modal-body").empty();
+        $("#modal-papers").empty();
+        
         $http.get("https://aws1617-01.herokuapp.com/api/v1/projectsbyuniversity/" + university, {}).then(function(response) {
             $scope.projects = response.data;
             for (var i = 0; i < $scope.projects.length; i++) {
@@ -68,6 +71,37 @@ angular.module("ResearcherListApp").controller("IntCtrl", function($scope, $http
             });
         });
 
+    }
+
+    $scope.loadPapers = function(orcid) {
+        console.log("Invoking https://aws1617-02.herokuapp.com/api/v1/researchers/" + orcid);
+        $http.get(
+            "https://aws1617-02.herokuapp.com/api/v1/researchers/" + orcid
+        ).then(function(response) {
+            $("#modal-header").html("<b>Researcher</b> | " + response.data[0].name);
+            $("#modal-body").html("<b>ORCID:</b> " + response.data[0].orcid + "<br><b>Name:</b> " + response.data[0].name + "<br><b>Phone:</b> " + response.data[0].phone + "<br><b>Email: </b><a href=\"mailto:" + response.data[0].email + "\">" + response.data[0].email + "</a><br><b>Address:</b> " + response.data[0].address + "<br><b>Gender: </b>" + response.data[0].gender + "<br>");
+            $("#myModal").modal();
+        });
+        
+        // ONLY WORKS IN US NETWORK
+        /*$http.get(
+            "https://api.elsevier.com/content/search/author?query=orcid(" + orcid + ")&apiKey=c3fc66dd92e97d5b54e49a58e001bdb1"
+        ).then(function(response) {
+            var auid = response.data["search-results"].entry[0]["dc:identifier"];
+            auid = auid.substring(10, auid.length);
+            $http.get(
+                "https://api.elsevier.com/content/search/scopus?query=au-id(" + auid + ")&apiKey=c3fc66dd92e97d5b54e49a58e001bdb1"
+            ).then(function(response) {
+                var papers = "<ul>";
+                for (var i = 0; i < response.data["search-results"].entry.length; i++) {
+                    var entry = response.data["search-results"].entry[i];
+                    papers += "<li><b><a href=https://www.scopus.com/inward/record.uri?partnerID=HzOxMe3b&scp=\"" + entry["eid"].substring(7, entry["eid"].length) + "\">" + entry["dc:title"] + "</a></b><ul><li>" + entry["prism:publicationName"] + "</li><li>Cited by " + entry["citedby-count"] + "</li></ul></li>";
+                    if (i == 5)
+                        break;
+                }
+                $("#modal-papers").html(papers + "</ul>");
+            });
+        });*/
     }
 
     $scope.loadUniversities();
